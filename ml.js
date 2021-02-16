bc9f794152d2dde8cf4d09537e6226ee1113a5c9
asabeneh <asabeneh@gmail.com>
2021-02-16 07:24:05 +0200

    inital commit

---
+++ ml.js
@@ -0,0 +1,17 @@
+const models = [
+  {
+    name: 'Linear Regression',
+  },
+  {
+    name: 'Logistic Regression',
+  },
+  {
+    name: 'Random Forest',
+  },
+  {
+    name: 'k-Nearest Neighbor(kNN)',
+  },
+  {
+    name: 'Ridge Regression',
+  },
+]
